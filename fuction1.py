92e88479ffa79982ba2e6161f55b862ed2441af4
macklinkim <kopserf@gmail.com>
2022-04-19 09:15:17 +0900

    second day

---
+++ fuction1.py
@@ -0,0 +1,23 @@
+# fuction1.py
+#함수를 정의
+def setValue(newValue):
+    x = newValue
+    print("함수내부:", x)
+
+
+#호출
+#디버깅할 때 중단점(Break Point)
+retValue = setValue(5)
+print(retValue)
+
+#교집합 문자를 리턴하는 함수
+def intersect(prelist, postlist):
+    #지역변수
+    result = []
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result 
+
+#호출
+print(intersect("HAM","SPAM"))
